6e63976d7c596e50028978529c3746ea323cf7b6
Rafael Camargo <hello@rafaelcamargo.com>
2021-12-24 15:49:24 -0300

    chore: setup project config for production

--- webpack.conf.dev.js
+++ webpack.conf.dev.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [new webpack.SourceMapDevToolPlugin()],
-  devServer: {host: '0.0.0.0', hot: true}
+  devServer: {host: '0.0.0.0', hot: true},
+  plugins: [
+    new webpack.SourceMapDevToolPlugin()
+  ]
 }

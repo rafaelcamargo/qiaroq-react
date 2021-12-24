9e0983f90447927db3743d779a6f978f6d59a019
Rafael Camargo <hello@rafaelcamargo.com>
2021-12-24 13:09:14 -0300

    chore: create credits component

---
+++ src/base/components/credits/credits.js
@@ -0,0 +1,11 @@
+import './credits.styl';
+import React from 'react';
+import avatarImageUrl from '@src/base/images/avatar-rafael-camargo.jpeg';
+import { Avatar } from '@src/base/components/avatar/avatar';
+
+export const Credits = () => (
+  <div className="credits">
+    <Avatar name="Rafael Camargo" imageUrl={avatarImageUrl} />
+    Maintained by <a href="https://rafaelcamargo.com" target="_blank" rel="noopener noreferrer">Rafael Camargo</a>
+  </div>
+)
---
+++ src/base/components/credits/credits.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Credits } from './credits';
+
+describe('Credits', () => {
+  function mount(){
+    return render(<Credits />);
+  }
+
+  it('should have appropriate css class', () => {
+    const { container } = mount({ name: 'Rafael Camargo' });
+    expect(container.firstChild).toHaveAttribute('class', 'credits');
+  });
+
+  it('should contain Rafael Camargo avatar', () => {
+    const { getByAltText } = mount();
+    const avatar = getByAltText('Rafael Camargo\'s avatar')
+    expect(avatar).toHaveAttribute('src', 'avatar-rafael-camargo.jpeg');
+  });
+
+  it('should contain a caption', () => {
+    const { getByText } = mount();
+    const link = getByText('Rafael Camargo');
+    expect(getByText('Maintained by')).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', 'https://rafaelcamargo.com');
+  });
+});

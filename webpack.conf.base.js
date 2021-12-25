fbb67c3da1dffee27758abb8c1eb16fe8210ee30
Rafael Camargo <hello@rafaelcamargo.com>
2021-12-25 09:57:59 -0300

    chore: setup favicon

--- webpack.conf.base.js
+++ webpack.conf.base.js
@@ -35,6 +35,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: project.source.index.file,
+      favicon: project.source.favicon.file,
       minify: {collapseWhitespace: true}
     }),
     new MiniCssExtractPlugin({
